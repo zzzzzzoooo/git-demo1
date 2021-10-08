28b62e422167b8269745cc70f5c71dfb5cf60579
zhang <zhang@qq.com>
2021-10-08 09:03:38 +0800

    第6次添加  hotfix  冲突

--- src/main/java/com/company/GitTest2.java
+++ src/main/java/com/company/GitTest2.java
@@ -4,5 +4,6 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("1111");
         System.out.println("hot-fix1");
+        System.out.println("hot-fix 冲突！");
     }
 }
